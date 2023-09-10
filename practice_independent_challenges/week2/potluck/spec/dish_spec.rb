85e38b880a287f0726e2761a6b5fa972c27f7383
Matt Holmes <78521446+matthewjholmes@users.noreply.github.com>
2023-09-09 17:59:59 -0700

    Iteration 1

--- practice_independent_challenges/week2/potluck/spec/dish_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/dish_spec.rb
@@ -3,4 +3,22 @@ require './lib/dish'
 
 describe Dish do 
 
+    # Iteration 1
+
+    describe '#initalize' do
+        it 'is a Dish' do 
+            dish = Dish.new("Couscous Salad", :appetizer)
+            expect(dish).to be_a Dish
+        end
+
+        it 'can return name' do 
+            dish = Dish.new("Couscous Salad", :appetizer)
+            expect(dish.name).to eq "Couscous Salad"            
+        end
+
+        it 'can return category' do
+            dish = Dish.new("Couscous Salad", :appetizer)
+            expect(dish.category).to eq :appetizer
+        end
+    end
 end
