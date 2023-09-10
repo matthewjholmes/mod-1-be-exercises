568d4323d1e669dc629d3e42b9addc5d5338822d
Matt Holmes <78521446+matthewjholmes@users.noreply.github.com>
2023-09-09 17:27:30 -0700

    Iteration 1

--- practice_independent_challenges/week1/venue/lib/venue.rb
+++ practice_independent_challenges/week1/venue/lib/venue.rb
@@ -1,3 +1,12 @@
 class Venue
+    attr_reader :name,
+                :capacity,
+                :patrons
+                
+    def initialize(name, capacity)
+        @name = name
+        @capacity = capacity
+        @patrons = []
+    end
 
 end
