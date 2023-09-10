30e508f0104681ef41b512e1991cf2c165068eb7
Matt Holmes <78521446+matthewjholmes@users.noreply.github.com>
2023-09-09 19:58:53 -0700

    Iteration 4

--- practice_independent_challenges/week2/potluck/lib/potluck.rb
+++ practice_independent_challenges/week2/potluck/lib/potluck.rb
@@ -16,4 +16,29 @@ class Potluck
             dish if dish.category == category
         end.compact
     end
+
+    def menu_categories
+        @dishes.map do |dish|
+            dish.category
+        end.uniq
+    end
+
+    def menu
+        unsorted_menu = {}
+        @dishes.each do |dish| 
+            unsorted_menu[dish.category] ||= []
+            unsorted_menu[dish.category] << dish.name
+        end
+        menu = {}
+        unsorted_menu.each do |category, dishes|
+            menu[category] = dishes.sort
+        end
+        menu
+    end
+
+    def ratio(category)
+        total_dish_count = @dishes.count.to_f
+        category_dish_count = get_all_from_category(category).count.to_f
+        (category_dish_count / total_dish_count) * 100
+    end
 end
--- practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
@@ -36,7 +36,7 @@ describe Potluck do
         end
     end
 
-    #Iteration 3
+    # Iteration 3
 
     describe '#get_all_from_category' do 
         it 'can filter potluck dishes by category' do
@@ -58,5 +58,54 @@ describe Potluck do
             expect(potluck.get_all_from_category(:appetizer).first.name).to eq "Couscous Salad"
         end
     end
+    
+    # Iteration 4
 
+    before(:each) do 
+        @potluck = Potluck.new("7-13-18")
+        couscous_salad = Dish.new("Couscous Salad", :appetizer)
+        summer_pizza = Dish.new("Summer Pizza", :appetizer)
+        roast_pork = Dish.new("Roast Pork", :entre)
+        cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+        candy_salad = Dish.new("Candy Salad", :dessert)
+        bean_dip = Dish.new("Bean Dip", :appetizer)
+        @potluck.add_dish(couscous_salad)
+        @potluck.add_dish(summer_pizza)
+        @potluck.add_dish(roast_pork)
+        @potluck.add_dish(cocktail_meatballs)
+        @potluck.add_dish(candy_salad)
+        @potluck.add_dish(bean_dip)
+    end
+
+    describe '#menu' do
+        it '#menu_categories helper method returns all represented categories as symbols' do
+            expect(@potluck.menu_categories).to eq [:appetizer, :entre, :dessert]
+            expect(@potluck.menu_categories.sample.class).to eq Symbol    
+        end
+
+        it '#menu returns a hash with categories as keys and dish names in arrays for values' do 
+            expect(@potluck.menu.class).to eq Hash
+            @potluck.menu_categories.each do |cat|
+                expect(@potluck.menu.include?(cat)).to eq true
+            end
+            @potluck.menu_categories.each do |cat|
+                expect(@potluck.menu[cat].class).to eq Array
+            end
+        end
+        
+        it 'menu categories are sorted alphabetically' do 
+            expect(@potluck.menu).to eq({:appetizer=>["Bean Dip", "Couscous Salad", "Summer Pizza"],:entre=>["Cocktail Meatballs", "Roast Pork"],:dessert=>["Candy Salad"]})
+        end
+    end
+
+    describe '#ratio' do
+        it 'calculates the decimal fraction of a category dish count to the potluck total dish count' do 
+            expect(@potluck.ratio(:appetizer)/100).to eq 0.5
+        end
+        
+        it 'packages ratio as percentage Float' do 
+            expect(@potluck.ratio(:appetizer).class).to eq Float
+            expect(@potluck.ratio(:appetizer)).to eq 50.0
+        end
+    end
 end
